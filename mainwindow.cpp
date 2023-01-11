f898c6f9d996e62700525d9b5ab6ca355d5e5878
osx user <gili.shemesh@gmail.com>
2023-01-11 17:09:45 +0200

    commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,46 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QScroller>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    filesystemModel.setIconProvider(&iconProvider);
+    filesystemModel.setRootPath("");
+    filesystemModel.setFilter(QDir::Hidden | QDir::NoDotAndDotDot | QDir::AllDirs | QDir::AllEntries);
+
+    ui->treeView->setModel(&filesystemModel);
+    QString rootPath = QDir::homePath() + "/.password-store";
+
+        if (!rootPath.isEmpty()) {
+            const QModelIndex rootIndex = filesystemModel.index(QDir::cleanPath(rootPath));
+            if (rootIndex.isValid())
+                ui->treeView->setRootIndex(rootIndex);
+        }
+
+        ui->splitter->setSizes(QList<int>({150  , 400}));//INT_MAX
+
+        // Demonstrating look and feel features
+        ui->treeView->setAnimated(false);
+        ui->treeView->setIndentation(20);
+        ui->treeView->setSortingEnabled(true);
+
+
+        //const QSize availableSize = ui->treeView->size(); //ui->treeView->screen()
+        //->availableGeometry().size();
+        //ui->treeView->resize(availableSize / 2);
+        ui->treeView->setColumnWidth(0, 200);
+        ui->treeView->setColumnWidth(1, 10);
+        ui->treeView->setColumnWidth(2, 10);
+
+
+        // Make it flickable on touchscreens
+        QScroller::grabGesture(ui->treeView, QScroller::TouchGesture);
+
+        ui->treeView->setWindowTitle(QObject::tr("Dir View"));
+
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileSystemModel>
+#include <QFileIconProvider>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +19,7 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QFileSystemModel filesystemModel;
+    QFileIconProvider iconProvider;
 };
 #endif // MAINWINDOW_H
