b51a9ce62ed878311cc89e47d946b093d1675913
osx user <gili.shemesh@gmail.com>
2023-01-28 16:03:22 +0200

    commit

---
+++ UiGuard.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <QWidget>
+#include <QInputEvent>
+
+class UiGuard : public QObject
+{
+public:
+    UiGuard(QWidget * ui)
+        : widget(ui)
+    {
+        widget->grabMouse();
+        widget->grabKeyboard();
+        widget->installEventFilter(this);
+    }
+
+    ~UiGuard() override
+    {
+        widget->releaseKeyboard();
+        widget->releaseMouse();
+    }
+
+    bool eventFilter(QObject *, QEvent * event) override
+    {
+        return dynamic_cast<QInputEvent *>(event);  // Eat up the input events
+    }
+
+private:
+    QWidget * widget;
+};
--- mainqmltype.h
+++ mainqmltype.h
@@ -9,32 +9,9 @@
 #include "GpgIdManageType.h"
 
 #include <QInputEvent>
+#include "UiGuard.h"
 
-class UiGuard : public QObject
-{
-public:
-    UiGuard(QWidget * ui)
-        : widget(ui)
-    {
-        widget->grabMouse();
-        widget->grabKeyboard();
-        widget->installEventFilter(this);
-    }
 
-    ~UiGuard() override
-    {
-        widget->releaseKeyboard();
-        widget->releaseMouse();
-    }
-
-    bool eventFilter(QObject *, QEvent * event) override
-    {
-        return dynamic_cast<QInputEvent *>(event);  // Eat up the input events
-    }
-
-private:
-    QWidget * widget;
-};
 
 class MainQmlType : public QObject
 {
