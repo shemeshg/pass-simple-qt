c648f9c2d05953dc223bc785ff19f45097be656f
osx user <gili.shemesh@gmail.com>
2023-02-08 09:01:35 +0200

    commit

--- config.h
+++ config.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #define PROJECT_NAME "pass-simple"
-#define PROJECT_VER  "0.8"
+#define PROJECT_VER  "0.9"
 #define PROJECT_VER_MAJOR "0"
-#define PROJECT_VER_MINOR "8"
+#define PROJECT_VER_MINOR "9"
 #define PTOJECT_VER_PATCH ""
 #define PROJECT_OS "MACOSX"
 
