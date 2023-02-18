347bb2f35d7b5aa4398115102bab562a9d947537
Diego Iastrubni <diegoiast@gmail.com>
2023-02-18 22:20:05 +0200

    Delete auto generated file

--- config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define PROJECT_NAME "pass-simple"
-#define PROJECT_VER  "0.24"
-#define PROJECT_VER_MAJOR "0"
-#define PROJECT_VER_MINOR "24"
-#define PTOJECT_VER_PATCH ""
-#define PROJECT_OS "MACOSX"
-
