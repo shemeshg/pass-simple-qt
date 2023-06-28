77b482ca5a0bd4442c67a7cbcb2449acb1306ede
shemeshg <gili.shemesh@gmail.com>
2023-06-28 18:00:18 +0300

    commit

--- deploy-mac.sh
+++ deploy-mac.sh
@@ -4,12 +4,12 @@ cmake --build .
 cmake --install .
 
 mv pass-simple.app "Pass simple.app"
-zip -r -y pass-simple-osx_0.53.0.zip ./Pass\ simple.app
+zip -r -y pass-simple-macos_0.56.0.zip ./Pass\ simple.app
 
-sha256sum pass-simple-osx_0.53.0.zip
+sha256sum pass-simple-macos_0.56.0.zip
 
 # sftp iuqwer9@frs.sourceforge.net
 
 # sftp> cd /home/frs/project/pass-simple
-# sftp> rm pass-simple-osx_0.42.0.dmg
-# sftp> put pass-simple-osx_0.43.0.dmg
+# sftp> rm pass-simple-osx_0.55.0.dmg
+# sftp> put pass-simple-macos_0.56.0.dmg
