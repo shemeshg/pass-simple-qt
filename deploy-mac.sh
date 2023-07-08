95d260a2672f55384b637a3177a054e10161bd5d
shemeshg <gili.shemesh@gmail.com>
2023-07-08 16:53:36 +0300

    commit

--- deploy-mac.sh
+++ deploy-mac.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-/Volumes/FAST/Qt/6.5.0/macos/bin/qt-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=. ../pass-simple-qt
+/Volumes/FAST/Qt/6.5.1/macos/bin/qt-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=. ../pass-simple-qt
 cmake --build .
 cmake --install .
 
 mv pass-simple.app "Pass simple.app"
-zip -r -y pass-simple-macos_0.56.0.zip ./Pass\ simple.app
+zip -r -y pass-simple-macos_0.61.0.zip ./Pass\ simple.app
 
-sha256sum pass-simple-macos_0.56.0.zip
+sha256sum pass-simple-macos_0.61.0.zip
 
 # sftp iuqwer9@frs.sourceforge.net
 
