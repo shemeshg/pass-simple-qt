bd83e7cb01928514f5e681e24cec44cb4063f5a1
Your Name <you@example.com>
2024-04-12 08:03:33 +0300

    MsWindows backSlash to slash fix

--- deploy-windows.cmd
+++ deploy-windows.cmd
@@ -7,11 +7,11 @@ R:
 cd \
 mkdir deployed
 cd deployed
-call D:\Qt\6.6.1\msvc2019_64\bin\qt-cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=. D:\projects\pass-simple-qt
+call D:\Qt\6.7.0\msvc2019_64\bin\qt-cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=. D:\projects\pass-simple-qt
 call ninja
-D:\Qt\6.6.1\msvc2019_64\bin\windeployqt.exe  --qmldir D:\projects\pass-simple-qt pass-simple.exe
+D:\Qt\6.7.0\msvc2019_64\bin\windeployqt.exe  --qmldir D:\projects\pass-simple-qt pass-simple.exe
 cd \
 move deployed pass-simple-windows-x64
-powershell -Command "Compress-Archive -Path r:\pass-simple-windows-x64 -DestinationPath pass-simple-qt-1.0.1_win_x64.zip"
+powershell -Command "Compress-Archive -Path r:\pass-simple-windows-x64 -DestinationPath pass-simple-qt-1.0.4_win_x64.zip"
 REM # rmdir /s/q pass-simple-windows-x64
 
